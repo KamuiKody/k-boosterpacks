dfc2e28954bc96e3d71ae5a0468f8cacc72ff84c
KamuiKody <62488881+KamuiKody@users.noreply.github.com>
2023-03-30 14:02:01 -0400

    Upload Manifest

---
+++ k-boosterpacks/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+ui_page 'html/index.html'
+
+client_scripts {
+    'client/*.lua'
+}
+
+files {
+    'html/*.js',
+    'html/*.html',
+    'html/*.css'
+}
+
+lua54 'yes'
